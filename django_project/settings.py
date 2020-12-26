31538330ae55197d4bac7da058d36e1a2d315dbf
Nicolas Solmayor <nicvsolgm@gmail.com>
2020-12-26 14:20:35 +0800

    deploying postgres

--- django_project/settings.py
+++ django_project/settings.py
@@ -72,8 +72,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'csipdi',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
